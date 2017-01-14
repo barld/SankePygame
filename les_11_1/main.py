5d2fc85548bde29e92cbac7a702048dd65308345
MilanSas <milansas1@gmail.com>
2017-01-14 22:05:25 +0100

    Update main.py

--- les_11_1/main.py
+++ les_11_1/main.py
@@ -1,31 +1,40 @@
 import pygame
 import sys
-
-from SnakeGame import Game
-
+from SnakeGameBarld import Game
 pygame.init()
 
 size = width, height = 500, 500
-
-black = 0,0,0
+clock = pygame.time.Clock()
+yellow = 255,255,0
+black = 0, 0, 0
+white = 255,255,255
+basicfont = pygame.font.Font('freesansbold.ttf', 18)
+displaysurf = pygame.display.set_mode((width, height))
 screen = pygame.display.set_mode(size)
 
-game = Game(20,20,500,500)
-clock = pygame.time.Clock()
+game = Game(20, 20, width, height)
+
+
+def drawScore(score):
+    scoreSurf = basicfont.render('Score: %s' % (score), True, white)
+    scoreRect = scoreSurf.get_rect()
+    scoreRect.topleft = (width - 120, 10)
+    displaysurf.blit(scoreSurf, scoreRect)
 
 while True:
-
     events = pygame.event.get()
 
     for event in events:
         if event.type == pygame.QUIT: sys.exit()
 
-    dt = clock.tick(120) / 1000.0
+    dt = clock.tick(60)/1000
 
     game.update(events, dt)
 
     screen.fill(black)
 
     game.draw(screen)
+
+    drawScore(game.score)
 
     pygame.display.flip()
